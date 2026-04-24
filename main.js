6105f35e592029d3ed176b82875371c2e09e8e93
goodlad1219 <nigam@NIGAMESH>
2026-04-25 00:09:03 +0530

    update v1

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 320,
+    height: 400,
+    transparent: true,    // True OS-level transparency
+    frame: false,         // Removes Windows title bar
+    alwaysOnTop: true,    // Floats over everything
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
